fd04f4ffb6a8953b28b84407d83573a5dec4b64e
Shruthi Chinnamsetty <chshruthi07@outlook.com>
2025-02-19 11:18:17 +0000

    added some guidance for the frontend directory

---
+++ frontend/src/components/component.tsx
@@ -0,0 +1,1 @@
+//all your components go here
---
+++ frontend/src/types/index.ts
@@ -0,0 +1,24 @@
+// all your export interface stuff goes here
+
+/*
+export interface Language {
+    languageId: number;
+    languageName: string;
+}
+
+export interface Category {
+    categoryId: number;
+    categoryName: string;
+}
+
+export interface Phrase {
+    phraseId: number;
+    languageId: number;
+    categoryId: number;
+    phraseText: string;
+    translation: string;
+    category?: Category;
+}
+
+heres an example
+*/
